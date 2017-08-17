043082e4cd6e7dc9c72dd8585fda4fe69b7b12ed
michael <545554463@qq.com>
2017-08-17 18:34:32 +0800

    Create CreateFileUtil.java

---
+++ src/main/java/com/tao/file/CreateFileUtil.java
@@ -0,0 +1,138 @@
+package com.tao;
+
+/**
+ * Created by Michael on 2017/8/17.
+ */
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 创建文件的工具类
+ */
+public class CreateFileUtil {
+
+
+    /**
+     * 创建文件
+     * @param destFileName - 绝对路径
+     * @return
+     */
+    public static boolean createFile(String destFileName) {
+
+        File file = new File(destFileName);
+        if(file.exists()) {
+            System.out.println("创建单个文件" + destFileName + "失败，目标文件已存在！");
+            return false;
+        }
+        if (destFileName.endsWith(File.separator)) {
+            System.out.println("创建单个文件" + destFileName + "失败，目标文件不能为目录！");
+            return false;
+        }
+        //判断目标文件所在的目录是否存在
+        if(!file.getParentFile().exists()) {
+            //如果目标文件所在的目录不存在，则创建父目录
+            System.out.println("目标文件所在目录不存在，准备创建它！");
+            if(!file.getParentFile().mkdirs()) {
+                System.out.println("创建目标文件所在目录失败！");
+                return false;
+            }
+        }
+        //创建目标文件
+        try {
+            if (file.createNewFile()) {
+                System.out.println("创建单个文件" + destFileName + "成功！");
+                return true;
+            } else {
+                System.out.println("创建单个文件" + destFileName + "失败！");
+                return false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("创建单个文件" + destFileName + "失败！" + e.getMessage());
+            return false;
+        }
+    }
+
+
+    /**
+     * 创建文件夹
+     * @param destDirName - 绝对路径
+     * @return
+     */
+    public static boolean createDir(String destDirName) {
+
+        File dir = new File(destDirName);
+        if (dir.exists()) {
+            System.out.println("创建目录" + destDirName + "失败，目标目录已经存在");
+            return false;
+        }
+        if (!destDirName.endsWith(File.separator)) {
+            destDirName = destDirName + File.separator;
+        }
+        //创建目录
+        if (dir.mkdirs()) {
+            System.out.println("创建目录" + destDirName + "成功！");
+            return true;
+        } else {
+            System.out.println("创建目录" + destDirName + "失败！");
+            return false;
+        }
+    }
+
+
+    /**
+     * 创建临时文件
+     * @param prefix
+     * @param suffix
+     * @param dirName
+     * @return
+     */
+    public static String createTempFile(String prefix, String suffix, String dirName) {
+        File tempFile = null;
+        if (dirName == null) {
+            try{
+                //在默认文件夹下创建临时文件
+                tempFile = File.createTempFile(prefix, suffix);
+                //返回临时文件的路径
+                return tempFile.getCanonicalPath();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("创建临时文件失败！" + e.getMessage());
+                return null;
+            }
+        } else {
+            File dir = new File(dirName);
+            //如果临时文件所在目录不存在，首先创建
+            if (!dir.exists()) {
+                if (!CreateFileUtil.createDir(dirName)) {
+                    System.out.println("创建临时文件失败，不能创建临时文件所在的目录！");
+                    return null;
+                }
+            }
+            try {
+                //在指定目录下创建临时文件
+                tempFile = File.createTempFile(prefix, suffix, dir);
+                return tempFile.getCanonicalPath();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("创建临时文件失败！" + e.getMessage());
+                return null;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+        String projectPath = System.getProperty("user.dir");
+        String dirPath = projectPath + "\\aa\\bb\\cc";
+        String fileName = dirPath + "\\text1.txt";
+
+        //创建文件夹
+        CreateFileUtil.createDir(dirPath);
+        //创建文件
+        CreateFileUtil.createFile(fileName);
+    }
+}
